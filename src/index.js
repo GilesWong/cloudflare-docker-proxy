be4a94156217e2e9bdb66556ef21ae8f0284c1b5
Giles <iwg@live.com>
2024-08-29 09:27:56 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.wwg.xyz": dockerHub,
+  "quay.wwg.xyz": "https://quay.io",
+  "gcr.wwg.xyz": "https://gcr.io",
+  "k8s-gcr.wwg.xyz": "https://k8s.gcr.io",
+  "k8s.wwg.xyz": "https://registry.k8s.io",
+  "ghcr.wwg.xyz": "https://ghcr.io",
+  "cloudsmith.wwg.xyz": "https://docker.cloudsmith.io",
+  "ecr.wwg.xyz": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.wwg.xyz": dockerHub,
 };
 
 function routeByHosts(host) {
